1362cede3178cb34d94d9967d04b9efe616746e8
gt-on-1234 <oscar.nuki@gaintheory.com>
2022-10-30 17:45:04 +0000

    increments version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="hicon",
-    version="0.1.1",
+    version="0.1.2",
     description="Create, use, edit and manage hierarchical configurations files with Python.",
     url="https://github.com/gt-on-1234/hicon",
     author="Oscar Nuki",
